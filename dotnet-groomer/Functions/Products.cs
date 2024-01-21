5242ce66d6e12b9dbaeb56c6df7866bacfe5e2b2
Maciej <maciej.chajda@gmail.com>
2024-01-21 15:49:28 +0100

    product crud

---
+++ dotnet-groomer/Functions/Products.cs
@@ -0,0 +1,131 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using dotnet_groomer.Models;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace dotnet_groomer.Functions
+{
+    public class Products
+    {
+        private readonly MyDbContext _context;
+
+        public Products(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        [FunctionName("GetProducts")]
+        public async Task<IActionResult> GetProducts(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request to fetch products from MySQL.");
+
+            List<Product> products = null;
+            try
+            {
+                products = await _context.Products.ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                return new NotFoundObjectResult(ex.Message);
+            }
+
+            return new OkObjectResult(products);
+        }
+
+        [FunctionName("PostProduct")]
+        public async Task<IActionResult> PostProduct(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var data = JsonConvert.DeserializeObject<Product>(requestBody);
+
+            Product product;
+            try
+            {
+                product = new Product { 
+                    Name = data.Name,
+                    Price = data.Price,
+                    Time = data.Time,
+                };
+
+                _context.Products.Add(product);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestObjectResult($"{ex.Message} // {ex.InnerException.Message}");
+            }
+
+            return new OkObjectResult(product);
+        }
+
+        [FunctionName("UpdateProduct")]
+        public async Task<IActionResult> UpdateProduct(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "UpdateProduct/{productId}")] HttpRequest req,
+            ILogger log, int? productId)
+        {
+            if (productId == null)
+            {
+                return new NotFoundObjectResult("product_id cannot be null");
+            }
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var data = JsonConvert.DeserializeObject<Product>(requestBody);
+
+            Product product;
+            try
+            {
+                product = await _context.Products.FindAsync(productId);
+
+                product.Name = data.Name;
+                product.Price = data.Price;
+                product.Time = data.Time;
+
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestObjectResult($"{ex.Message} // {ex.InnerException.Message}");
+            }
+
+            return new OkObjectResult(product);
+        }
+
+        [FunctionName("DeleteProduct")]
+        public async Task<IActionResult> DeleteProduct(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "DeleteProduct/{productId}")] HttpRequest req,
+            ILogger log, int? productId)
+        {
+            if (productId == null)
+            {
+                return new NotFoundObjectResult("product_id cannot be null");
+            }
+
+            Product product;
+            try
+            {
+                product = await _context.Products.FindAsync(productId);
+
+                _context.Remove(product);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestObjectResult($"{ex.Message} // {ex.InnerException.Message}");
+            }
+
+            return new OkObjectResult("Product was removed correctly");
+        }
+    }
+}
